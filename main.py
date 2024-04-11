620dfcbdfbc9117624b88240f697d2f546ceb5ea
Dexter Mack <Dextermack111@gmail.com>
2024-04-11 14:19:33 -0700

    Name generator name project

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Python Band Name Generator")
+
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What's your pet's name? ")
+
+print(f"Your NBA exspansion name could be {city_name} {pet_name}")
